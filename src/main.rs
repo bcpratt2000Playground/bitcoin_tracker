8c7ae3fbc42abcbc062b2761ac5d75fddc79b8fc
bcpratt2000 <bcpratt2000@gmail.com>
2021-01-07 00:42:19 -0700

    running writes the price in bitcoin to the screen every minute

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use futures::executor::block_on;
+use std::thread;
+use std::time::Duration;
+use yahoo_finance_api::*;
 fn main() {
-    println!("Hello, world!");
+    let connector = YahooConnector::new();
+    let response = connector.get_latest_quotes("BTC-USD", "");
+    let quote = block_on(response).unwrap().last_quote().unwrap();
+    println!("Bitcoin Price: ${}", quote.close);
+    loop {
+        let response = connector.get_latest_quotes("BTC-USD", "");
+        thread::sleep(Duration::from_secs_f32(10.0));
+        let quote = block_on(response).unwrap().last_quote().unwrap();
+        println!("Bitcoin Price: ${}", quote.close);
+    }
 }
